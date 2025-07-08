826d791e395931dc10c80da4bc5b579cf2a056fe
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-08 23:29:27 +0530

    code

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const post("/signup",function(req, res){
+
+
+})
+app.post("/signin", function(req, res){
+
+})
+
+app.get("/signup", function(req, res){
+
+})
