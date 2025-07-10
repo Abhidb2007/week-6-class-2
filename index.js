32df8e4485f9d411cbade922c27fa4724297a15f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-10 13:01:19 +0530

    code

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/signin", function(req, res){
     const username = req.body.username;
     const password = req.body.password;
     let foundUser = null;
-    for(let i=0; i<users.length,i++){
+    for(let i=0; i<users.length;i++){
         if(users[i].username === username && users[i].password === password){
             foundUser = user[i]
         }
