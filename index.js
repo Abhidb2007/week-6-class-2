0df578e5918c83beb2750c3791b617076993544d
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-10 13:11:05 +0530

    code

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-const express = require('express');
+const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-const users = []
 
-app. post("/signup",function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    users.push({
-        username: username,
-        password: password
-    })
-    //we should check if the user already exists
-    res.json({
-        message: "you are signed in"
+app.use(express.json()); // <-- Needed to parse JSON body
 
-    })
+const JWT_SECRET = "my-secret-key"; // <-- Define secret key
+const users = [];
 
-})
-app.post("/signin", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    let foundUser = null;
-    for(let i=0; i<users.length;i++){
-        if(users[i].username === username && users[i].password === password){
-            foundUser = user[i]
-        }
-    }
-    if(!foundUser){
-        res.json({
-            message: "Credintials are incorrect"
-        })
-    }else{
-        const token  = jwt.sign({
-            username:"raman"
-           },   JWT_SECRET);
-        res.json({
-            
-            token: token
-        })    
-    }  
-})
+// SIGN UP Route
+app.post("/signup", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.get("/me", function(req, res){
-    const token = req.headers.token;
+  // Check if user already exists
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  users.push({ username, password });
+
+  res.json({ message: "You are signed up" });
+});
+
+// SIGN IN Route
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!foundUser) {
+    return res.json({ message: "Credentials are incorrect" });
+  }
+
+  const token = jwt.sign({ username: username }, JWT_SECRET);
+  res.json({ token });
+});
+
+// PROTECTED Route - /me
+app.get("/me", function (req, res) {
+  const token = req.headers.token;
+
+  try {
     const decodedData = jwt.verify(token, JWT_SECRET);
-    if(decodedData.username){
+    const username = decodedData.username;
 
+    const foundUser = users.find((user) => user.username === username);
+    if (!foundUser) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    if(decodedData.username){
-        let foundUser = null;
-        for(let i=0; i<users.length; i++){
-            if(users[i].username === username&& users[i].password === password){
-                foundUser = users[i];
-            }
-        }
-        res.json({
-            username: foundUser.username,
-            password: foundUser.password
-        })
-    }
+    res.json({
+      username: foundUser.username,
+      password: foundUser.password,
+    });
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
 
-
-})
-app.listen(3000);
-
-
-
-
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
