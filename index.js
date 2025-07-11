b315ae28795cad18279e86f857a30429511dad22
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-11 17:56:26 +0530

    code

---
+++ index.js
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        
+        <title>auth website</title>
+    </head>   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.1/axios.min.js"></script> 
+    <script>
+        async function signin(){
+            const username = document.getElementById
+            ("signin-username").value;
+            const password = document.getElementById
+            ("signin-password").value;
+            const response = await axios.post("http://localhost:3000/sigin",{
+                username: username,
+                password: password
+            });
+
+            localStorage.setItem("token",response.data.token);
+            alert("you are signed in");
+
+        }
+    </script>
+        <body>            
+            <div>
+                Signup
+                <input id="signup-username"  type="text" name="username"
+                placeholder="Username">
+                <input id="signup-password" type="password" name="password" placeholder="Password">
+                <button onclick= "signup()">Submit</button>
+
+
+            </div>
+            <div>
+                Signin 
+                <input id="signin-username" type="text" name="username" placeholder="username">
+                <input id="signin-password" type="password" name="password" placeholder="Password">
+                
+                <button onclick="signin()">Submit</button>
+            </div>
+            <div>
+                User information:
+                <div id="information"></div>
+
+            </div>
+            <div>
+                <button onclick="logout()">Logout</botton>
+            </div>
+
+        </body>
+
+    
+</html>
